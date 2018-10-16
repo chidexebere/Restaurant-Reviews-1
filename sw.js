1acc4f9e630e13567d2855c5e25d6fbe37f9f5e9
Chidiebere <chidex5@gmail.com>
2018-10-16 17:42:53 +0100

    Modify Cached Resources

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 
 /*Instlall the  service worker and Caches the resources*/
 
-const staticCacheName = 'restaurant-static-v1';
+const staticCacheName = 'restaurant-static-v12';
 
 self.addEventListener('install', event => {
   event.waitUntil(
@@ -9,13 +9,22 @@ self.addEventListener('install', event => {
       .then(cache => {
         return cache.addAll([
           '/index.html',
-          '/restaurant.html',
           '/css/styles.css',
           '/js/dbhelper.js',
-          '/js/registerSW.js',
+          '/js/register_sw.js',
           '/js/main.js',
           '/js/restaurant_info.js',
           '/data/restaurants.json',
+          '/restaurant.html?id=1',
+          '/restaurant.html?id=2',
+          '/restaurant.html?id=3',
+          '/restaurant.html?id=4',
+          '/restaurant.html?id=5',
+          '/restaurant.html?id=6',
+          '/restaurant.html?id=7',
+          '/restaurant.html?id=8',
+          '/restaurant.html?id=9',
+          '/restaurant.html?id=10',
         ]).catch(error => {
           console.log('Caches open failed: ' + error);
         });
