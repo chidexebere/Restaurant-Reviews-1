9ba31ff2063e5cbd186c48cd6c6874b0039eda34
Chidiebere <chidex5@gmail.com>
2018-12-07 10:46:43 +0100

    feat: Correct syntax mistake in JS

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+
+    "globals": {
+        "DBHelper": false,
+        "google": false
+    },
+    "parserOptions": {
+        "ecmaVersion": 2015,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "warn",
+            "tab",
+            { "SwitchCase": 1 }
+        ],
+        "linebreak-style": [
+            "warn",
+            "windows"
+        ],
+        "quotes": [
+            "warn",
+            "single"
+        ],
+        "semi": [
+            "warn",
+            "always"
+        ],
+        "no-console": "off",
+        "no-unused-vars": "warn",
+        "no-undef": "warn",
+        "no-useless-escape": "warn"
+    }
+};
+
+
